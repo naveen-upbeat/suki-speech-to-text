69803735ec502effaeee7a99ba538888a1dba7d4
Naveen <naveen.upbeat@gmail.com>
2023-04-29 04:34:34 +0800

    optimize: optimze the recording utils

--- libs/suki-api-configs/src/lib/suki-api-configs.spec.ts
+++ libs/suki-api-configs/src/lib/suki-api-configs.spec.ts
@@ -1,17 +1,27 @@
 import {
-  sukiApiConfigs,
+  getMessageKeyForStreamStart,
+  getMessageKeyForStreamStop,
   WEB_SOCKET_BATCH_PATH,
   WEB_SOCKET_STREAM_PATH,
 } from './suki-api-configs';
 
 describe('sukiApiConfigs', () => {
-  it('should work', () => {
-    expect(sukiApiConfigs()).toEqual('suki-api-configs');
-  });
   it('has WEB_SOCKET_BATCH_PATH', () => {
     expect(WEB_SOCKET_BATCH_PATH).toBeDefined();
   });
   it('has WEB_SOCKET_STREAM_PATH', () => {
     expect(WEB_SOCKET_STREAM_PATH).toBeDefined();
+  });
+
+  describe('has method getMessageKeyForStreamStart', () => {
+    it('getMessageKeyForStreamStart - is defined', () => {
+      expect(getMessageKeyForStreamStart).toBeDefined();
+    });
+  });
+
+  describe('has method getMessageKeyForStreamStop', () => {
+    it('getMessageKeyForStreamStop - is defined', () => {
+      expect(getMessageKeyForStreamStop).toBeDefined();
+    });
   });
 });
